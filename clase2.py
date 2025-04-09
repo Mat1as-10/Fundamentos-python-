b54bf5b56cbc4bda97992ba00154e121d4207ee3
Mat1as-10 <tomasmatias986@gmail.com>
2025-04-09 09:43:50 -0400

    Add files via upload

---
+++ clase2.py
@@ -0,0 +1,34 @@
+N01 = int(input("ingresa un numero: "))
+N02 = int(input("ingresa el segundo numero: "))
+
+R01 = N01 + N02 
+print ("El resultado de tu suma es: ", R01)
+
+
+
+
+P01 = int(input("ingresa un numero: "))
+P02 = int(input("Ingresa el segundo numero: ")) 
+
+R02 = P01 - P02 
+print (f"El resultado de tu resta es: {R02}")
+
+
+
+
+A01 = int(input("Ingresa tu nuemero: "))
+A02 = int(input("Ingresa tu segundo numero: "))
+
+R03 = A01 * A02
+print (f"El resultado de tu multiplicacion es: {R03}")
+
+
+
+
+B01 = int(input("Ingresa un numero: "))
+B02 = int(input("Ingresa el segundo numero: "))
+
+R04 = B01 / B02
+print (f"El resultado de tu division es: {R04}")
+
+
