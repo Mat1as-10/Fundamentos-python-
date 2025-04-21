14aeda0fbeff12ae206f0cebd009dfd8df2bef7d
Mat1as-10 <tomasmatias986@gmail.com>
2025-04-21 11:30:27 -0400

    Add files via upload

--- ejercicios.py
+++ ejercicios.py
@@ -3,17 +3,20 @@
 # Alumnos con calificaciones mayores a 5.0 obtendran un dcto del 15% 
 # Alumnos con calificaciones mayores a 4.0 recibiran solo un 10% 
 # El que no cumpla con estos requisitos lo ivitamos a seguir esforzandose... 
+def descuento ():
+
+    Nombre = (input("Bienvenido, ingresa tu nombre: "))
+
+    Nota = float(input(f"Bienvenido {Nombre} , ingresa tu calificacion: "))
+
+    if Nota >= 6.0:
+     print ("Excelente, has obtenido un descuento del 20%")
+    elif Nota >= 5.0:
+        print ("Felicitaciones, has obtenido un descuento del 15%")
+    elif Nota >= 4.0:
+     print ("Bien has obtenido un descuento del 10%")
+    else: 
+        print ("Has reprobado, sigue intentandolo. ") 
 
 
-Nombre = (input("Bienvenido, ingresa tu nombre: "))
-
-Nota = float(input(f"Bienvenido {Nombre} , ingresa tu calificacion: "))
-
-if Nota > 4.0:
-    print ("Bien, has obtenido un descuento del 10%")
-elif Nota > 5.0:
-    print ("Felicitaciones, has obtenido un descuento del 15%")
-elif Nota > 6.0:
-    print ("Excelente, Felicidades has obtenido un descuento del 20%")
-else: 
-    print ("Has reprobado, sigue intentandolo. ") 
+descuento ()
