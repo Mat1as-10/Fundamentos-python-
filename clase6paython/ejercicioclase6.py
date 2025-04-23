0c37ab88fbac459f99e2d1f02fce282b71b9df73
Mat1as-10 <tomasmatias986@gmail.com>
2025-04-23 09:39:43 -0400

    Update ejercicioclase6.py
    Ejercicio

---
+++ clase6paython/ejercicioclase6.py
@@ -0,0 +1,11 @@
+#La tienda falabella necesita desarrolar un programa para saber cuantos puntos tiene acumulado cada usuario, 
+# por cada $1.000 pesos que realice una compra un usuario recibe 10 puntos. 
+# Cuando un usuario alcance el nivel premium que son 1.000 puntos recibe un descuento del 25% 
+# Cuando un usuario reciba la puntuacion de 500 puntos estaria en el nive oro, por lo tanto recibe un descuento del 10%. 
+# Para los osuario que tengas 250 puntos no recibira descuentos, pero so grandes premios como audifonos, manos libres, relojes y mucho mas. 
+# Acontinuacion dictare los perfiles de tres usuarios 
+# Marcos tiene 1500 puntos a canjear, que niveles le corresponden;
+# Jose luis tiene 2500 puntos, que niveles le corresponderian;
+# Sebastian solo tiene 300 puntos.
+# Mostrar en pantalla la informacion que coresponda al usuario, el nivel que le corresponde de ser 1 2 o 3 niveles... en el que se encuentre segun susu puntos
+# al final darle un mensaje que lo motive a seguir comprando.
