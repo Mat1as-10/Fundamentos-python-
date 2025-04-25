1f0f3fa51e994330a21d2b1cfcd83aa0bbc9021b
Mat1as-10 <tomasmatias986@gmail.com>
2025-04-25 11:24:17 -0400

    Add files via upload

---
+++ Nueva carpeta/2.py
@@ -0,0 +1,6 @@
+Nombre = input("Ingresa tu nombre: ")
+
+def Saludo(Nombre):
+    print (f"Hola, {Nombre}. Bienvenido")
+
+Saludo(Nombre)
---
+++ Nueva carpeta/return.py
@@ -0,0 +1,10 @@
+A = int(input("Ingresa un numero: "))
+B = int(input("Ingresa el segundo numero: "))
+
+def Suma():
+    return A + B
+Resultado = A + B
+print ("El resultado es: ", Resultado)
+
+
+Suma()
